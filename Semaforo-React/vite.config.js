2f9a3f65848fe12867c126b5786ab925a4b41058
Roymar Castillo <roymarcastillo@estudiantec.cr>
2025-11-15 09:21:10 -0600

    correcion vite config

--- Semaforo-React/vite.config.js
+++ Semaforo-React/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Semaforos-con-react",
+  base: "/Semaforos-con-react/Semaforo-react",
   plugins: [react()],
 })
